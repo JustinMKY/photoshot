1ee96cc3277d89ea86173e83f52f572a2743785a
JustinMKY <64981306+JustinMKY@users.noreply.github.com>
2023-07-10 15:27:19 -0400

    test mailer [do]

--- src/core/mailer.ts
+++ src/core/mailer.ts
@@ -1,46 +1,28 @@
 import { render } from "mjml-react";
+import nodemailer from "nodemailer";
 import { ReactElement } from "react";
 
-const nodemailer = require("nodemailer");
-
 export const EMAIL_SUBJECTS = {
-  LOGIN: "Your Photoshot Login Link",
+  LOGIN: "Your Login Link",
 };
 
-const transporter = nodemailer.createTransport({
-  port: process.env.EMAIL_PORT,
-  host: process.env.EMAIL_SERVER,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-  secure: true,
-});
+const transporter = nodemailer.createTransport(process.env.EMAIL_SERVER);
 
-const sendMessage = async (message: string) => {
-  const mailData = {
-    from: {
-      name: "Photoshot",
-      address: process.env.EMAIL_FROM,
-    },
-    replyTo: "no-reply@test.com",
-    to: process.env.TO_EMAIL,
-    subject: EMAIL_SUBJECTS.LOGIN,
-    text: message,
-    html: message,
-  };
+export const sendEmail = async ({
+  to,
+  subject,
+  component,
+}: {
+  to: string;
+  subject: string;
+  component: ReactElement;
+}) => {
+  const { html } = render(component);
 
-  await transporter.sendMail(mailData);
-};
-
-export default async (req: any, res: any) => {
-  const { message } = req.body;
-
-  try {
-    await sendMessage(message);
-    res.status(200).json({ status: "OK" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  await transporter.sendMail({
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html,
+  });
 };

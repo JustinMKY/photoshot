15ac883e9f6bda1dfcef306ad4edc5389217d745
JustinMKY <64981306+JustinMKY@users.noreply.github.com>
2023-07-12 14:38:48 -0400

    console log mailer

--- src/core/mailer.ts
+++ src/core/mailer.ts
@@ -15,17 +15,23 @@ const sendMessage = async (message: string) => {
     html: message,
   };
 
+  console.log("Sending message:", msg); // Console log the message object before sending
+
   await sgMail.send(msg);
+  console.log("Message sent successfully"); // Console log after sending
 };
 
 export default async (req: any, res: any) => {
   const { message } = req.body;
 
+  console.log("Received request with message:", message); // Console log the received message
+
   try {
     await sendMessage(message);
+    console.log("Email sent successfully"); // Console log after email is sent
     res.status(200).json({ status: "OK" });
   } catch (error) {
-    console.error(error);
+    console.error("Error sending email:", error); // Console log the error if sending fails
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
